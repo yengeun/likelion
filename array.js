4c62830ae3c90f9c41a491dec894fccdff6ed2ef
yengeun <7613ez@gmail.com>
2024-03-26 20:23:48 +0900

    week1

---
+++ array.js
@@ -0,0 +1,6 @@
+const arr=[100,[200,300],["javascript","jquery"]];
+
+console.log(arr[0]);
+console.log(arr[1][0]);
+console.log(arr[1][1]);
+console.log(arr[2][0]);
---
+++ if.js
@@ -0,0 +1,14 @@
+//조건문, 삼항연산자
+const num=100;
+
+if(num==100){
+    document.write("실행되었습니다.(true)");
+}
+
+if(num==100)document.write("실행되었습니다.(true)");
+
+if(num==100)document.write("실행되었습니다.(true)");
+else document.write("실행되었습니다.(false)");
+
+//삼항연산자
+(num==100)?document.write("true") : document.write("false");
