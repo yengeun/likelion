be7d85948d80949ce1f9be58f159ccb5c2ff261a
yengeun <7613ez@gmail.com>
2024-04-01 14:31:02 +0900

    1주차 자기소개과제

---
+++ likelion_week1/switch.js
@@ -0,0 +1,13 @@
+const num = 100;
+
+switch(num){
+    case 90:
+        document.write("실행되었습니다.(90)");
+        break;
+    case 95:
+        document.write("실행되었습니다.(95)");
+        break;
+    case 100:
+        document.write("실행되었습니다.(100)");
+        break;
+}
