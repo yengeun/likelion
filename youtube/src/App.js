43a14e20fff17a8d728126185bfb6a0844ab67b1
yeung-eung <7613ez@gmail.com>
2024-07-08 15:31:33 +0900

    컴퍼넌트 비동기 작업; 오류확인필요

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,39 +1,35 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './components/section/Main';
+import Baseball from './pages/Baseball';
+import { BiHealth } from 'react-icons/bi';
 
-import Home from './pages/Home'
-import Today from './pages/Today'
-import Baseball from './pages/Baseball'
-import Health from './pages/Health'
-import Port from './pages/Port'
-import Channel from './pages/Channel'
-import Video from './pages/Video'
-import Search from './pages/Search'
-import Not from './pages/Not'
-
-import Header from './components/section/Header';   //추가
-import Main from './components/section/Main';       //추가
-import Footer from './components/section/Footer';   //추가
-import { BiBaseball } from 'react-icons/bi';
+const Home = lazy(() => import('./pages/Home'));
+const Today = lazy(() => import('./pages/Today'));
+const Developer = lazy(() => import('./pages/Baseball'));
+const Webd = lazy(() => import('./pages/Health'));
+const Port = lazy(() => import('./pages/Port'));
+const Channel = lazy(() => import('./pages/Channel'));
+const Video = lazy(() => import('./pages/Video'));
+const Search = lazy(() => import('./pages/Search'));
+const Not = lazy(() => import('./pages/Not'));
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Header />
-            <Main>
+            <Suspense fallback={<Main />}>
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path="/today" element={<Today />} />
                     <Route path="/baseball" element={<Baseball />} />
                     <Route path="/health" element={<Health />} />
-                   
+                    <Route path="/port" element={<Port />} />
                     <Route path='/channel/:channelId' element={<Channel />} />
                     <Route path='/video/:videoId' element={<Video />} />
                     <Route path='/search/:searchId' element={<Search />} />
                     <Route path="*" element={<Not />} />
                 </Routes>
-            </Main>
-            <Footer />
+            </Suspense>
         </BrowserRouter>
     );
 }
