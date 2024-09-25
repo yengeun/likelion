b5e4d0375515f5302b6221cb9b2ff5d0b6425159
yengeun <124172198+yengeun@users.noreply.github.com>
2024-09-25 14:48:21 +0900

    Feat:로딩페이지

--- likelion_0924TEST/src/App.js
+++ likelion_0924TEST/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './components/Main'
 import Header from './components/Section/Header'
@@ -7,8 +7,26 @@ import Loading from './components/Loading'
 import Like from './components/Like'
 import Search from './components/Search'
 import Mypage from './components/Mypage'
+import './assets/scss/setting/common.scss';
+
 
 const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate a delay to show the loading page
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000); // Adjust the time delay as needed
+
+        return () => clearTimeout(timer); // Cleanup timer on unmount
+    }, []);
+
+    if (isLoading) {
+        return <Loading />;  // Show the loading page while loading
+    }
+
+
     return (
         <BrowserRouter>
             <Header />
@@ -21,7 +39,7 @@ const App = () => {
             </Routes>
             <Nav />
         </BrowserRouter>
-    )
-}
+    );
+};
 
 export default App
