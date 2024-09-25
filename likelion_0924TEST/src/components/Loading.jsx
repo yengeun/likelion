8f21b0356f54e7aa80fc7fb013f3e7b1b1cff537
yengeun <124172198+yengeun@users.noreply.github.com>
2024-09-25 14:47:51 +0900

    Feat:로딩페이지

--- likelion_0924TEST/src/components/Loading.jsx
+++ likelion_0924TEST/src/components/Loading.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import '../assets/scss/section/_loading.scss';
+import logo from '../assets/img/icon/Logo.svg';  // Import the logo SVG
 
 const Loading = () => {
-    return (
-        <div className='Loading_wrap container'>
-            Loading
-        </div>
-    )
-}
+  return (
+    <div className="loading-container">
+      <img src={logo} alt="Loading logo" className="loading-logo" />
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
