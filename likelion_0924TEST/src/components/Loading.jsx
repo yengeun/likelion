c3fd5a38a494416c9ccc79265c46a40be3aa11b9
yengeun <7613ez@gmail.com>
2024-09-25 13:58:11 +0900

    add file

---
+++ likelion_0924TEST/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className='Loading_wrap container'>
+            Loading
+        </div>
+    )
+}
+
+export default Loading
---
+++ likelion_0924TEST/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Slider from './Section/Slider'
+
+const Main = () => {
+    return (
+        <div className='Main_wrap container'>
+            <Slider />
+            Main
+        </div>
+    )
+}
+
+export default Main
