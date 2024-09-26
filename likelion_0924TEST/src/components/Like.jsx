ae49a3f169f8e83a4235ed9efb0b89d4f77e2a9b
yengeun <7613ez@gmail.com>
2024-09-26 10:05:47 +0900

    Feat:like,mypage완성

--- likelion_0924TEST/src/components/Like.jsx
+++ likelion_0924TEST/src/components/Like.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Like = () => {
     return (
         <div className='Like_wrap container'>
-            like
+            로그인 후 사용이 가능합니다
         </div>
     )
 }
--- likelion_0924TEST/src/components/Mypage.jsx
+++ likelion_0924TEST/src/components/Mypage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const Mypage = () => {
     return (
         <div className='Mypage_wrap container'>
-            Mypage
+            로그인 후 사용이 가능합니다
         </div>
     )
 }
