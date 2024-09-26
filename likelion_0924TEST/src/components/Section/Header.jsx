569904734e2ab2cf90456c82e4c7f298b31ad178
yengeun <7613ez@gmail.com>
2024-09-26 09:51:53 +0900

    Feat:header,nav구성

--- likelion_0924TEST/src/components/Section/Header.jsx
+++ likelion_0924TEST/src/components/Section/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import logo from '../../assets/img/icon/Logowhite.svg'
+import '../../assets/scss/section/_section.scss'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header className="header">
+        <img src={logo} alt="성신여자대학교 로고" className="header-logo" />
+        </header>
     )
 }
 
--- likelion_0924TEST/src/components/Section/Nav.jsx
+++ likelion_0924TEST/src/components/Section/Nav.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import '../../assets/scss/section/_section.scss'
+
+import homeIcon from '../../assets/img/icon/home.svg';
+import homeFullIcon from '../../assets/img/icon/homefull.svg';
+import likeIcon from '../../assets/img/icon/like.svg';
+import likeFullIcon from '../../assets/img/icon/likefull.svg';
+import searchIcon from '../../assets/img/icon/search.svg';
+import searchFullIcon from '../../assets/img/icon/searchfull.svg';
+import myPageIcon from '../../assets/img/icon/mypage.svg';
+import myPageFullIcon from '../../assets/img/icon/mypagefull.svg';
 
 const Nav = () => {
+    const location = useLocation(); // 현재 경로 가져오기
+
     return (
-        <div>Nav</div>
-    )
-}
+        <nav className="nav">
+            <Link to="/" className="nav-item">
+                <img 
+                    src={location.pathname === "/" ? homeFullIcon : homeIcon} 
+                    alt="Home" 
+                />
+            </Link>
+            <Link to="/like" className="nav-item">
+                <img 
+                    src={location.pathname === "/like" ? likeFullIcon : likeIcon} 
+                    alt="Like" 
+                />
+            </Link>
+            <Link to="/search" className="nav-item">
+                <img 
+                    src={location.pathname === "/search" ? searchFullIcon : searchIcon} 
+                    alt="Search" 
+                />
+            </Link>
+            <Link to="/mypage" className="nav-item">
+                <img 
+                    src={location.pathname === "/mypage" ? myPageFullIcon : myPageIcon} 
+                    alt="My Page" 
+                />
+            </Link>
+        </nav>
+    );
+};
 
 export default Nav
